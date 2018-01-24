13b84c82075cf8298d9d7c38ddff257242f1e05f
Robert Kwiatkowski <rjk2147@columbia.edu>
2018-01-24 18:20:11 -0500

    updated densenet script

--- run_densenet.sh
+++ run_densenet.sh
@@ -1,52 +1,47 @@
 # Clonning the densenet Repo
-git clone https://github.com/gpleiss/efficient_densenet_pytorch
-cd efficient_densenet_pytorch
-rm demo.py
+git clone https://github.com/bamos/densenet.pytorch
+pip3 install setproctitle, graphviz
+cd densenet.pytorch
 wget https://raw.githubusercontent.com/Columbia-Creative-Machines-Lab/Learning-Rate-Normalization/master/sgd_lr_norm.py -O sgd_lr_norm.py
-wget https://raw.githubusercontent.com/Columbia-Creative-Machines-Lab/Learning-Rate-Normalization/master/densenet_demo.py -O demo.py
-pip3 install fire
+wget https://raw.githubusercontent.com/Columbia-Creative-Machines-Lab/Learning-Rate-Normalization/master/adam_lr_norm.py -O adam_lr_norm.py
+rm train.py
+wget https://raw.githubusercontent.com/Columbia-Creative-Machines-Lab/Learning-Rate-Normalization/master/densenet_train.py -O train.py
 
 # Baselines
-# SGD
-sed -i '/optimizer = optim./c\    optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=wd)' demo.py
-echo "SGD_CIFAR"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data cifar10 --save SGD_CIFAR | tee sgd_cifar.txt
-echo "SGD_SVHN"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data svhn --save SGD_SVHN | tee sgd_svhn.txt
-
-# SGD+Momentum 
-sed -i '/optimizer = optim./c\    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, nesterov=True, weight_decay=wd)' demo.py
+# SGD+Momentum
 echo "SGD_momentum_CIFAR"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data ciraf10 --save SGD_momentum_CIFAR | tee sgd_momentum_cifar.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt sgd --data cifar | tee sgd_momentum_cifar.txt
 echo "SGD_momentum_SVHN"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data svhn --save SGD_momentum_SVHN | tee sgd_momentum_svhn.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt sgd --data svhn | tee sgd_momentum_svhn.txt
 
 # Adam
-sed -i '/optimizer = optim./c\    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=wd)' demo.py
 echo "Adam_CIFAR"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data cifar10--save Adam_CIFAR | tee adam_cifar.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt adam --data cifar | tee adam_cifar.txt
 echo "Adam_SVHN"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data svhn --save Adam_SVHN | tee adam_svhn.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt adam --data svhn | tee adam_svhn.txt
 
 # RMSProp
-sed -i '/optimizer = optim./c\    optimizer = optim.RMSprop(model.parameters(), lr=lr, momentum=momentum, weight_decay=wd)' demo.py
 echo "RMSPROP_CIFAR"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data cifar10--save RMSProp_CIFAR | tee rmsprop_cifar.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt rmsprop --data cifar | tee rmsprop_cifar.txt
 echo "RMSPROP_SVHN"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data svhn --save RMSProp_SVHN | tee rmsprop_svhn.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt rmsprop --data svhn | tee rmsprop_svhn.txt
 
 # Adadelta
-sed -i '/optimizer = optim./c\    optimizer = optim.Adadelta(model.parameters(), lr=lr, weight_decay=wd)' demo.py
 echo "Adadelta_CIFAR"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data cifar10--save Adadelta_CIFAR | tee adadelta_cifar.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt adadelta --data cifar | tee adadelta_cifar.txt
 echo "Adadelta_SVHN"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data svhn --save Adadelta_SVHN | tee adadelta_svhn.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt adadelta --data svhn | tee adadelta_svhn.txt
 
 # SGD_LR_Norm
 echo "SGD_lr_norm_momentum_CIFAR"
-sed -i '/optimizer = optim./c\    optimizer = optim.SGD_lr_norm(model.parameters(), lr=lr, momentum=momentum, nesterov=True, weight_decay=wd)' demo.py
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data cifar10--save SGD_lr_norm_momentum_CIFAR | tee sgd_lr_norm_cifar.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt sgd_lr_norm --data cifar | tee sgd_lr_norm_cifar.txt
 echo "SGD_lr_norm_momentum_SVHN"
-CUDA_VISIBLE_DEVICES=0 python3 demo.py --efficient True --data svhn --save SGD_lr_norm_momentum_SVHN | tee sgd_lr_norm_svhn.txt
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt sgd_lr_norm --data svhn | tee sgd_lr_norm_svhn.txt
+
+# Adam_LR_Norm
+echo "Adam_lr_norm_momentum_CIFAR"
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt adam_lr_norm --data cifar | tee adam_lr_norm_cifar.txt
+echo "Adam_lr_norm_momentum_SVHN"
+CUDA_VISIBLE_DEVICES=0 python3 train.py --opt adam_lr_norm --data svhn | tee adam_lr_norm_svhn.txt
 
 echo "Densenet experiments finished"
